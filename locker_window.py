60f8a5690cef111b87283cb8bc6a067907c21bac
ann <inkina0508@yandex.ru>
2024-01-16 18:23:17 +0300

    локер без картинок скинов

--- locker_window.py
+++ locker_window.py
@@ -1,4 +1,5 @@
 import pygame
+from btn import Button
 
 
 class Locker:
@@ -6,12 +7,17 @@ class Locker:
         self.screen = screen
         self.settings = settings
         self.buttons = pygame.sprite.Group()
+        self.create_buttons()
+
+    def create_buttons(self):
+        Button(self.screen, self.settings, self.buttons, (150, 150, 150),
+               450, 450, 300, 100, 'TRY IT',
+               self.change_clothes())
 
     def update(self):
         self.screen.blit(self.settings.locker_bg, (0, 0))
+        for button in self.buttons:
+            button.update()
 
     def change_clothes(self):
-        pass
-
-    def save_avatar(self):
         pass
